03a020e9884bc8996b2d583a4dbc63f56348f19a
Clemens Stadlbauer <ccstadlbauer@posteo.de>
2016-01-18 10:52:22 +0100

    Bindet Menü Musik ein

--- src/main_menu.py
+++ src/main_menu.py
@@ -1,5 +1,7 @@
 import wx
 
+import audio
+
 
 class MyFrame(wx.Frame):
 
@@ -15,20 +17,34 @@ class MyFrame(wx.Frame):
 
         gs = wx.GridSizer(4, 1, 5, 5)
 
-        btn_main_menu = wx.Button(self, -1, 'Main Menu')
-        btn_endless = wx.Button(self, -1, 'Endless Mode')
-        btn_controls = wx.Button(self, -1, 'Controls')
-        btn_credits = wx.Button(self, -1, 'Credits')
+        self.btn_main_menu = wx.Button(self, -1, 'Main Menu')
+        self.btn_endless = wx.Button(self, -1, 'Endless Mode')
+        self.btn_controls = wx.Button(self, -1, 'Settings')
+        self.btn_credits = wx.Button(self, -1, 'Credits')
+
+        self.Bind(wx.EVT_BUTTON, self.OnButton, self.btn_main_menu)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, self.btn_endless)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, self.btn_controls)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, self.btn_credits)
 
         gs.AddMany([
-            (btn_main_menu, 0, wx.EXPAND),
-            (btn_endless, 0, wx.EXPAND),
-            (btn_controls, 0, wx.EXPAND),
-            (btn_credits, 0, wx.EXPAND)
+            (self.btn_main_menu, 0, wx.EXPAND),
+            (self.btn_endless, 0, wx.EXPAND),
+            (self.btn_controls, 0, wx.EXPAND),
+            (self.btn_credits, 0, wx.EXPAND)
         ])
 
         box.Add(gs, 1, wx.EXPAND)
         self.SetSizer(box)
+
+    def OnButton(self, event):
+        if event.GetEventObject() is self.btn_main_menu:
+            audio.play('../assets/music/OGG files/menu.ogg')
+        elif event.GetEventObject() is self.btn_credits:
+            audio.play('../assets/music/OGG files/credits.ogg')
+        else:
+            audio.stop()
+
 
 if __name__ == '__main__':
     app = wx.App()
