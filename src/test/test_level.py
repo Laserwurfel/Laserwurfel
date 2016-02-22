55fee0e9b9d4250319ad75e4ea45def207553533
Clemens Stadlbauer <ccstadlbauer@posteo.de>
2016-02-22 11:29:44 +0100

    Erweitert Tests des Level Parsers

--- src/test/test_level.py
+++ src/test/test_level.py
@@ -7,6 +7,10 @@ class StandardLevelTestCase(unittest.TestCase):
     def setUp(self):
         self.here = os.path.abspath(os.path.dirname(__file__))
 
+    def test_full(self):
+        full = level.Level()
+        full.parse(os.path.join(self.here, 'level/full'))
+
     def test_meta(self):
         with open(os.path.join(self.here, 'level/meta')) as f:
             meta = level.parse_meta(f)
@@ -28,6 +32,25 @@ class StandardLevelTestCase(unittest.TestCase):
                         level.structure.path,
                         (-1, -1, +1),
                         (+1, -1, -1),
+                    ),
+                    (
+                        level.structure.deactivate,
+                        (-1, 00, +1),
+                    ),
+                    (
+                        level.structure.connection,
+                        (+1, 00, +1),
+                        (+1, 00, -1),
+                    ),
+                    (
+                        level.structure.wall,
+                        (+1, -1, 00),
+                        (+1, -1, -1),
+                    ),
+                    (
+                        level.structure.teleporter,
+                        (-1, +1, +1),
+                        (00, +1, -1),
                     ),
                 ],
             )
@@ -125,3 +148,64 @@ class StandardLevelTestCase(unittest.TestCase):
                 ],
                 "top details different",
             )
+
+
+class BrokenDetailTestCase(unittest.TestCase):
+    def setUp(self):
+        self.here = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'level/details',
+        )
+
+    @unittest.expectedFailure
+    def test_7x7(self):
+        with open(os.path.join(self.here, '7x7')) as f:
+            level.parse_detail(f)
+
+    @unittest.expectedFailure
+    def test_missing_column(self):
+        with open(os.path.join(self.here, 'missing_column')) as f:
+            level.parse_detail(f)
+
+    @unittest.expectedFailure
+    def test_missing_delimiter_long(self):
+        with open(os.path.join(self.here, 'missing_delimiter_long')) as f:
+            level.parse_detail(f)
+
+    @unittest.expectedFailure
+    def test_missing_delimiter_short(self):
+        with open(os.path.join(self.here, 'missing_delimiter_short')) as f:
+            level.parse_detail(f)
+
+    @unittest.expectedFailure
+    def test_missing_edge(self):
+        with open(os.path.join(self.here, 'missing_edge')) as f:
+            level.parse_detail(f)
+
+
+class BrokenStructureTestCase(unittest.TestCase):
+    def setUp(self):
+        self.here = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'level/structures',
+        )
+
+    @unittest.expectedFailure
+    def test_invalid_points(self):
+        with open(os.path.join(self.here, 'invalid_points')) as f:
+            level.parse_structure(f)
+
+    @unittest.expectedFailure
+    def test_no_arguments(self):
+        with open(os.path.join(self.here, 'no_arguments')) as f:
+            level.parse_structure(f)
+
+    @unittest.expectedFailure
+    def test_unkown_function(self):
+        with open(os.path.join(self.here, 'unknown_function')) as f:
+            level.parse_structure(f)
+
+    @unittest.expectedFailure
+    def test_wrong_arguments(self):
+        with open(os.path.join(self.here, 'wrong_arguments')) as f:
+            level.parse_structure(f)
